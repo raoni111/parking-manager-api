bf5587daf60eddf3e6c9cd5d046b7ac543c92359
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-08-24 15:12:41 -0300

    create-crypt-password

--- app/utils/crypt_password.py
+++ app/utils/crypt_password.py
@@ -1,7 +1,14 @@
 from bcrypt import (
     hashpw,
     gensalt,
+    checkpw
 );
 
-def crypt_password(password: str):
+def crypt_password(password: str) -> bytes:
     return hashpw(password.encode("utf-8"), gensalt());
+
+def check_password(password: str, passwordHashed: str) -> bool:
+    return checkpw(
+        password,
+        passwordHashed
+    );
