38cd0cf059f68a64169b8ad8d4106bcd54511a8b
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2024-09-27 08:08:18 -0300

    create-update-delete-and-getOne

--- app/models/parking.py
+++ app/models/parking.py
@@ -24,35 +24,75 @@ class Parking():
             return {
                 "success": True,
                 "messages": [
-                    "Dados do estacionamento registrado com sucesso."
+                    "Dados do estacionamento registrado com sucesso.",
                 ]
             };
         except:
             return {
                 "success": False,
                 "messages": [
-                    "Não foi possível registrar os dados do estacionamento."
+                    "Não foi possível registrar os dados do estacionamento.",
                 ]
             };
 
     async def update(parking: Dict[str, str], parkingId: str):
-        parking_ref = db.collection(f'parking/{parkingId}');
+        parking_ref = db.collection('parking').document(parkingId);
 
         date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S");
         
-        parking["update_at"] = date;
+        parking["updated_at"] = date;
     
         try:
-            parking_ref
+            parking_ref.update(parking);
+        
+            return {
+                "success": True,
+                "messages": [
+                    "Dados do estacionamento editado com sucesso.",
+                ]
+            }
         except: 
             return {
                 "success": False,
                 "messages": [
-                    
+                    "Não foi possível editar os dados do estacionamento.",
                 ]
             }
 
-        
+    async def delete(parkingId: str):
+        parking_ref = db.collection('parking').document(parkingId);
+
+        try:
+            parking_ref.delete();
+    
+            return {
+                "success": True,
+                "messages": [
+                    "Dados deletado com sucesso",
+                ]
+            }
+        except: 
+            return {
+                "success": False,
+                "messages": [
+                    "Não foi possível deletar os dados do parking"
+                ]
+            }
+
+    async def getOne(parkingId: str): 
+        parking_ref = db.collection("parking").document(parkingId);
+    
+        try:
+            response = parking_ref.get();
+    
+            return response;
+        except:
+            return {
+                "success": False,
+                "messages": [
+                    "Não foi possível encontrar os dados do estacionamento"
+                ]
+            }
 
     @staticmethod
     async def get():
